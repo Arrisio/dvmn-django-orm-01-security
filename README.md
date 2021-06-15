957b34c34064198611e4de30b87a237c99a5f2cc
Dmitriy Suchkov <Account.log@yandex.ru>
2021-06-15 12:03:36 +0300

    feat: add db connection settings

---
+++ README.md
@@ -0,0 +1,9 @@
+#Курс "Знакомство с Django: ORM" 
+##Задача "Урок 1. Пишем пульт охраны банка"
+
+Ссылка на урок: https://dvmn.org/modules/django-orm/lesson/watching-storage
+исходник: https://github.com/dvmn-tasks/django-orm-watching-storage
+
+Изменено:
+- Код из начальных шагов урока добавлен как комментарий `main.py`
+- Добавлены настройки подключения к бд в `settings.py`
--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from django.core.management import execute_from_command_line
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 execute_from_command_line("manage.py runserver 0.0.0.0:8000".split())
+
+
+# код с предыдущих шагов
+# print("Всего пропусков:", Passcard.objects.count())  # noqa: T001
+# print("Активных пропусков (фильтр app):", len([passcard for passcard in Passcard.objects.all() if passcard.is_active]))
+# print("Активных пропусков (фильтр бд):", Passcard.objects.filter(is_active=True).count())  # noqa: T001
